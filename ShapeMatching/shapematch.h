89410d199097c32c1d184488416044b67f9e1544
HyeWon <maru_05@naver.com>
2017-11-15 18:42:25 +0900

    Shape Matching

---
+++ ShapeMatching/shapematch.h
@@ -0,0 +1,11 @@
+#ifndef _shapematch_h
+#define _shapematch_h
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+
+Mat ShapeMatching(Mat, Mat, Mat);
+
+#endif
